ed1f4cfd097a4382d1e9b9b53d71b8d9826a1f7f
Oskar Stenberg <01ste02@utb.lund.se>
2019-11-11 16:19:21 +0100

    Add project files.

---
+++ Prov/MainActivity.cs
@@ -0,0 +1,121 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using static Android.Views.View;
+using Android.Views;
+using Java.Lang;
+using Android.Content;
+
+namespace Prov
+{
+    [Activity(Label = "Prov", MainLauncher = true)]
+    public class MainActivity : Activity, IOnClickListener
+    {
+        Button btnSin;
+        Button btnCos;
+        Button btnTan;
+        Button btnCTan;
+
+        EditText txtInput;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+            
+            btnSin = FindViewById<Button>(Resource.Id.btnSin);
+            btnCos = FindViewById<Button>(Resource.Id.btnCos);
+            btnTan = FindViewById<Button>(Resource.Id.btnTan);
+            btnCTan = FindViewById<Button>(Resource.Id.btnCTan);
+
+            btnSin.SetOnClickListener(this);
+            btnCos.SetOnClickListener(this);
+            btnTan.SetOnClickListener(this);
+            btnCTan.SetOnClickListener(this);
+
+            txtInput = FindViewById<EditText>(Resource.Id.txtInput);
+        }
+
+        public void OnClick(View v)
+        {
+            if (v.Id == btnSin.Id)
+            {
+                if (!txtInput.Text.ToString().Equals("") && txtInput.Text != null)
+                {
+                    double num = Double.ParseDouble(txtInput.Text.ToString());
+                    num = num * Math.Pi / 180;
+                    num = Math.Sin(num);
+
+                    StartResult(num);
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter operand", ToastLength.Long).Show();
+                }
+            }
+            else if (v.Id == btnCos.Id)
+            {
+                if (!txtInput.Text.ToString().Equals("") && txtInput.Text != null)
+                {
+                    double num = Double.ParseDouble(txtInput.Text.ToString());
+                    num = num * Math.Pi / 180;
+                    num = Math.Cos(num);
+
+                    StartResult(num);
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter operand", ToastLength.Long).Show();
+                }
+            }
+            else if (v.Id == btnTan.Id)
+            {
+                if (!txtInput.Text.ToString().Equals("") && txtInput.Text != null)
+                {
+                    double num = Double.ParseDouble(txtInput.Text.ToString());
+                    num = num * Math.Pi / 180;
+                    num = Math.Tan(num);
+
+                    StartResult(num);
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter operand", ToastLength.Long).Show();
+                }
+            }
+            else if (v.Id == btnCTan.Id)
+            {
+                if (!txtInput.Text.ToString().Equals("") && txtInput.Text != null)
+                {
+                    double num = Double.ParseDouble(txtInput.Text.ToString());
+
+                    if (num != 0 && num != 0.0)
+                    {
+                        num = num * Math.Pi / 180;
+                        num = 1 / Math.Tan(num);
+
+                        StartResult(num);
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Not ctan with 0", ToastLength.Long).Show();
+                    }
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter operand", ToastLength.Long).Show();
+                }
+            }
+        }
+
+        public void StartResult(double resultNum)
+        {
+            Intent intent = new Intent(this, typeof(ResultScreenActivity));
+            intent.PutExtra("resultNumber", resultNum);
+
+            StartActivity(intent);
+        }
+    }
+}
+
---
+++ Prov/ResultScreenActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Prov
+{
+    [Activity(Label = "ResultScreenActivity")]
+    public class ResultScreenActivity : Activity
+    {
+        TextView txtResult;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.resultScreen);
+
+            ActionBar.Hide();
+
+            txtResult = FindViewById<TextView>(Resource.Id.txtResult);
+
+            txtResult.Text = Intent.GetDoubleExtra("resultNumber", 0.0).ToString();
+        }
+    }
+}
